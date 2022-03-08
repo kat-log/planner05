285c4a73707393f8a19d11d13edc6fe668e244b2
kat_log <greentea.tomatosystem@gmail.com>
2022-03-08 22:15:24 +0900

    #7 Modefy: puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,13 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+
+#EC2デプロイのためコメントアウト
+#port ENV.fetch("PORT") { 3000 }
+
+#EC2デプロイのため追記 planner05として
+bind "unix:///var/www/planner05/tmp/sockets/puma.sock"
+
 
 # Specifies the `environment` that Puma will run in.
 #
