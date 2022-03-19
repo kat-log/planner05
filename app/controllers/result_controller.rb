7eb248080a312f2b72b8bd9248f0071349a46950
kat_log <greentea.tomatosystem@gmail.com>
2022-03-20 06:32:36 +0900

    Add: #9 表示結果を3つに限定

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,7 +1,7 @@
 class ResultController < ApplicationController
   def result_page
     if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
     end
   end
 end
