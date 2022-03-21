f72abc6ae2ea6ff1e98fc7e7af75173fd27fcdca
kat_log <greentea.tomatosystem@gmail.com>
2022-03-22 06:44:39 +0900

    Add: #11 ranking_by_age変数をコントローラーへ

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -3,5 +3,6 @@ class ResultController < ApplicationController
     if params[:keyword]
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
     end
+    @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => 20, :sex => 1)
   end
 end
