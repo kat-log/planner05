c30c5b5a5ea084ba5c06b2b17c8aee3a4a4a921f
kat_log <greentea.tomatosystem@gmail.com>
2022-03-25 06:15:29 +0900

    Add: #11 性別変数を@変数に

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,9 +1,9 @@
 class ResultController < ApplicationController
   def result_page
-    sex = 0
+    @sex = 1
     if params[:keyword]
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
     end
-    @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => 20, :sex => sex)
+    @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => 20, :sex => @sex)
   end
 end
