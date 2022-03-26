e4fa6b85fae53995d523653bff73610147e8061a
kat_log <greentea.tomatosystem@gmail.com>
2022-03-26 19:27:21 +0900

    Delete: #11 試しコードを削除

--- app/controllers/choose_controller.rb
+++ app/controllers/choose_controller.rb
@@ -1,7 +1,4 @@
 class ChooseController < ApplicationController
   def choose_page
-    if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
-    end
   end
 end
--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -2,20 +2,8 @@ class ResultController < ApplicationController
 
   # プルダウンから受け取った値を元に楽天から結果を取得
   def result_page
-
-    @sex = 0
-    if params[:sex]
-      @sex = params[:sex]
-    end
-
-    @age = 10
-    if params[:age]
-      @age = params[:age]
-    end
-
-    if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword]+"プレゼント", hits: 3)
-    end
+    @sex = params[:sex]
+    @age = params[:age]
     @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => @age, :sex => @sex)
   end
 end
