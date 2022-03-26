f0a56a59bc2e718cbb0842b98537fee44d0c2e02
kat_log <greentea.tomatosystem@gmail.com>
2022-03-26 13:01:10 +0900

    Add: #11 年代もプルダウンで選べるように実装

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -2,10 +2,20 @@ class ResultController < ApplicationController
 
   # プルダウンから受け取った値を元に楽天から結果を取得
   def result_page
-    @sex = params[:sex]
-    if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
+
+    @sex = 0
+    if params[:sex]
+      @sex = params[:sex]
     end
-    @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => 20, :sex => @sex)
+
+    @age = 10
+    if params[:age]
+      @age = params[:age]
+    end
+
+    if params[:keyword]
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword]+"プレゼント", hits: 3)
+    end
+    @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => @age, :sex => @sex)
   end
 end
