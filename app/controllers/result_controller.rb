cf3dea28f955ebe3d65e93cd0f9841b74442071d
kat_log <greentea.tomatosystem@gmail.com>
2022-05-03 05:27:12 +0900

    Trying: #69 YouTubeAPIにてタイトル取得実装中

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -10,5 +10,20 @@ class ResultController < ApplicationController
     require 'news-api'
     news = News.new(ENV['NEWS_API_KEY'])
     @news = news.get_top_headlines(country: 'jp', pageSize: 3)
+
+    #YouTube動画を取得
+    require 'google/apis/youtube_v3'
+
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = Rails.application.credentials.google[:api_key]
+
+    options = {
+      :id => 'UCPyNsNSTUtywkekbDdCA_8Q' #YouTubeチャンネルのIDを指定
+    }
+
+    response = youtube.list_channels(:snippet, id: options)
+
+    @channel_title = response[:items][0][:snippet][:title]
+
   end
 end
