d7ab786dd97427775a3b219ad797e744d398b444
kat_log <greentea.tomatosystem@gmail.com>
2022-05-13 07:32:42 +0900

    CREATE: #70 result_pageアクション内の処理をAPIごとのメソッドに移行

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -17,27 +17,28 @@ class ResultController < ApplicationController
   end
 
   def result_page
+    pull_rakuten_item
+    pull_news
+    pull_youtube_video
+  end
+
+  def pull_rakuten_item
     # プルダウンから受け取った値を元に楽天から結果を取得
     @sex = params[:sex]
     @age = params[:age]
     @ranking_by_age = RakutenWebService::Ichiba::Item.ranking(:age => @age, :sex => @sex)
+  end
 
+  def pull_news
     # ニュースを取得
     require 'news-api'
     news = News.new(ENV['NEWS_API_KEY'])
     @news = news.get_top_headlines(country: 'jp', pageSize: 3)
-
-    # YouTube動画を取得
-    @youtube_data = find_videos('インディアンス')
-  end
-
-  def pull_rakuten_item
-  end
-
-  def pull_news
   end
 
   def pull_youtube_video
+    # YouTube動画を取得
+    @youtube_data = find_videos('インディアンス')
   end
 
 end
